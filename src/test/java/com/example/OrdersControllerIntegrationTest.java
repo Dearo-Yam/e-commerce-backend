75a3079e543bd2071b0a2ebab6fb7e4404f971f5
MingQing Zhu <leozhu333@gmail.com>
2022-09-28 22:22:08 -0700

    trying to merge from dev

--- src/test/java/com/example/OrdersControllerIntegrationTest.java
+++ src/test/java/com/example/OrdersControllerIntegrationTest.java
@@ -3,15 +3,30 @@ package com.example;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = {com.example.demo.DemoApplication.class})
 public class OrdersControllerIntegrationTest {
+    private static String baseURI = "http://localhost:8080/api/orders";
+    private String path;
+
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = baseURI;
+    }
+
+    @Test
+    public void testHello() {
+        path = "/hello";
+        RequestSpecification request = RestAssured.given();
+        Response response = request.get(path);
+        Assertions.assertEquals("Hello from Order controller", response.asPrettyString());
+    }
+
 //    @Test
-//    public void test() {
-//        RestAssured.baseURI = "http://localhost:8080/api/orders/hello";
-//        RequestSpecification request = RestAssured.given();
-//        request.get().getBody().prettyPrint();
-//    }
+//    public void
 }
